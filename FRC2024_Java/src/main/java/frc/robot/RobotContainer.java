719af8276f8a42175ca277c30e3675f6fd8d2ae7
Lev Strougov <lstrougov@gmail.com>
2024-01-10 20:13:03 -0500

    is this good?

--- FRC2024_Java/src/main/java/frc/robot/RobotContainer.java
+++ FRC2024_Java/src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,8 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Outtake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -26,9 +28,12 @@ public class RobotContainer {
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
+  private final Intake intake;
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
+    this.intake = new Intake();  
     configureBindings();
   }
 
---
+++ FRC2024_Java/src/main/java/frc/robot/utils/OuttakeState.java
@@ -0,0 +1,5 @@
+package frc.robot.utils;
+
+public enum OuttakeState {
+    FORWARD,BACKWARD,STOPPED
+}
