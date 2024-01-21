983bcb2c615d006d1498506817cb547b2679b797
goatfanboi23 <dev.codetoad@gmail.com>
2024-01-21 14:50:33 -0500

    started to add poses

--- src/main/java/frc/robot/autochooser/FieldLocation.java
+++ src/main/java/frc/robot/autochooser/FieldLocation.java
@@ -4,17 +4,19 @@ import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.RobotContainer;
 
 public enum FieldLocation {
-     Right(0),
-     Middle(0),
-     Left(0);
+     Right(0,0),
+     Middle(1.25,5.50),
+     Left(0,0);
      private static final double RED_X_POS = 16.5; //meters
      private final double yPos;
+     private final double xPose;
 
-     FieldLocation(double yPos) {
+     FieldLocation(double xPos, double yPos) {
+          this.xPose = xPos;
           this.yPos = yPos;
      }
      public Translation2d getLocation(){
-          double x = RobotContainer.shouldFlip() ? RED_X_POS : 0;
+          double x = RobotContainer.shouldFlip() ? RED_X_POS - xPose * 2: xPose;
           return new Translation2d(x, yPos);
      }
 }
