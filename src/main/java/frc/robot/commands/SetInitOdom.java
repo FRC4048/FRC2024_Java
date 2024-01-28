aac2ea9f8602b4bbde278b7ace2b97de9f653a54
goatfanboi23 <dev.codetoad@gmail.com>
2024-01-28 14:11:53 -0500

    got rid of unused method

--- src/main/java/frc/robot/commands/SetInitOdom.java
+++ src/main/java/frc/robot/commands/SetInitOdom.java
@@ -15,16 +15,9 @@ public class SetInitOdom extends Command {
 
     @Override
     public void initialize() {
-//        double newOffset = rangeConvert(-180, 180, -360, 360, chooser.getStartingPosition().getRotation().getDegrees());
         drivetrain.setGyroOffset(chooser.getStartingPosition().getRotation().getDegrees()*-1);
         drivetrain.resetOdometry(chooser.getStartingPosition());
     }
-    private static double rangeConvert(double startMin, double startMax, double endMin, double endMax, double value){
-        double oldRange = startMax - startMin;
-        double newRange = endMax - endMin;
-        return (((value - startMin) * newRange) / oldRange) + endMin;
-    }
-
     @Override
     public boolean isFinished() {
         return true;
