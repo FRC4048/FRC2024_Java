aeac919f2e506b8f7960e65b8bdafe654d9ceaea
goatfanboi23 <dev.codetoad@gmail.com>
2024-02-03 13:04:21 -0500

    made auto and driving work for red

--- src/main/java/frc/robot/Constants2024.java
+++ src/main/java/frc/robot/Constants2024.java
@@ -10,8 +10,8 @@ public class Constants2024 extends GameConstants {
     public static final int RAMP_ID = 45;
 
     //Shooter
-    public static final int SHOOTER_MOTOR_ID_1 = 44;
-    public static final int SHOOTER_MOTOR_ID_2 = 45;
+    public static final int SHOOTER_MOTOR_ID_1 = 4400; //needs to be changed
+    public static final int SHOOTER_MOTOR_ID_2 = 4500; //needs to be changed
 
     public static final int SHOOTER_SENSOR_ID_1 = 0;
     public static final int SHOOTER_SENSOR_ID_2 = 1;
