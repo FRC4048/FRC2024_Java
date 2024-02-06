432b13b44c0bf68c47ab2f3272ab0a212cc988ce
goatfanboi23 <dev.codetoad@gmail.com>
2024-02-06 18:38:42 -0500

    removed todo because branch that implemented red odometry was merged

--- src/main/java/frc/robot/subsystems/swervev2/SwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/swervev2/SwerveDrivetrain.java
@@ -1,24 +1,19 @@
 package frc.robot.subsystems.swervev2;
 
 import com.kauailabs.navx.frc.AHRS;
-
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.swervev2.components.EncodedSwerveSparkMax;
 import frc.robot.subsystems.swervev2.type.GenericSwerveModule;
-import frc.robot.utils.smartshuffleboard.SmartShuffleboard;
 
-/**
- * blue centric //TODO make work for red
- */
+
 public class SwerveDrivetrain extends SubsystemBase {
     
     private final GenericSwerveModule frontLeft;
