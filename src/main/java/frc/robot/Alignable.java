c198d374260a4973b437f64add4b2aff163326a6
goatfanboi23 <dev.codetoad@gmail.com>
2024-02-12 09:52:27 -0500

    made changes from comments

--- src/main/java/frc/robot/Alignable.java
+++ src/main/java/frc/robot/Alignable.java
@@ -5,21 +5,23 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import java.util.function.BiFunction;
 
 public enum Alignable {
-    AMP(0,0,(x,y)-> new Rotation2d(Math.PI / 2)),
-    SPEAKER(0.46,5.67,(x2,y2)-> new Rotation2d(RobotContainer.shouldFlip() ? 0 : Math.PI).plus(new Rotation2d(Math.atan(y2/x2))));
+    AMP(0,0,0,(x,y)-> new Rotation2d(Math.PI / 2)),
+    SPEAKER(0.46,16.46,5.67,(x2,y2)-> new Rotation2d(RobotContainer.isRedAlliance() ? 0 : Math.PI).plus(new Rotation2d(Math.atan(y2/x2))));
 
     private final double blueX;
+    private final double redX;
     private final double y;
     private final BiFunction<Double, Double, Rotation2d> functionFromDist;
 
-    Alignable(double blueX, double y, BiFunction<Double,Double,Rotation2d> functionFromDist) {
+    Alignable(double blueX, double redX, double y, BiFunction<Double,Double,Rotation2d> functionFromDist) {
         this.blueX = blueX;
+        this.redX = redX;
         this.y = y;
         this.functionFromDist = functionFromDist;
     }
 
     public double getX() {
-        return RobotContainer.shouldFlip() ? blueX + 16 : blueX;
+        return RobotContainer.isRedAlliance() ? redX : blueX;
     }
 
     public double getY() {
@@ -28,11 +30,5 @@ public enum Alignable {
 
     public Rotation2d getAngle(double x, double y) {
         return functionFromDist.apply(x - getX(), y - getY());
-    }
-    public double calcTurnSpeed(Rotation2d currentAngle, double x, double y){
-        double diff = currentAngle.minus(getAngle(x,y)).getDegrees();
-        if (Math.abs(diff) < 2) return 0;
-        return (Math.abs(diff)) / (360) * Math.signum(diff);
-
     }
 }
--- src/main/java/frc/robot/Drive.java
+++ src/main/java/frc/robot/Drive.java
@@ -4,6 +4,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.swervev2.SwerveDrivetrain;
+import frc.robot.utils.AutoAlignment;
 
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
@@ -14,30 +15,31 @@ public class Drive extends Command {
     private final DoubleSupplier fwdSupplier;
     private final DoubleSupplier strSupplier;
     private final DoubleSupplier rtSupplier;
-    private final Supplier<Alignable> autoAlign;
+    private final Supplier<Alignable> autoAim;
     private boolean shouldFlip;
 
-    public Drive(SwerveDrivetrain drivetrain, DoubleSupplier fwdSupplier, DoubleSupplier strSupplier, DoubleSupplier rtSupplier, Supplier<Alignable> autoAlign) {
+    public Drive(SwerveDrivetrain drivetrain, DoubleSupplier fwdSupplier, DoubleSupplier strSupplier, DoubleSupplier rtSupplier, Supplier<Alignable> autoAim) {
         this.drivetrain = drivetrain;
         this.fwdSupplier = fwdSupplier;
         this.strSupplier = strSupplier;
         this.rtSupplier = rtSupplier;
-        this.autoAlign = autoAlign;
+        this.autoAim = autoAim;
         addRequirements(drivetrain);
     }
 
     @Override
     public void initialize() {
-        this.shouldFlip = RobotContainer.shouldFlip();
+        this.shouldFlip = RobotContainer.isRedAlliance();
     }
 
     @Override
     public void execute() {
+        Alignable alignable = autoAim.get();
         double fwd = MathUtil.applyDeadband(fwdSupplier.getAsDouble()*Constants.MAX_VELOCITY,0.3);
         double str = MathUtil.applyDeadband(strSupplier.getAsDouble()*Constants.MAX_VELOCITY, 0.3);
         double rcw;
-        if (autoAlign.get() == null) rcw = MathUtil.applyDeadband(rtSupplier.getAsDouble() * Constants.MAX_VELOCITY, 0.3);
-        else rcw = autoAlign.get().calcTurnSpeed(drivetrain.getPose().getRotation(),drivetrain.getPose().getX(),drivetrain.getPose().getY()) * Constants.MAX_ANGULAR_SPEED;
+        if (alignable == null) rcw = MathUtil.applyDeadband(rtSupplier.getAsDouble() * Constants.MAX_VELOCITY, 0.3);
+        else rcw = AutoAlignment.calcTurnSpeed(alignable, drivetrain.getPose()) * Constants.MAX_ANGULAR_SPEED;
         ChassisSpeeds driveStates = drivetrain.createChassisSpeeds(fwd*(shouldFlip ? 1 : -1), str*(shouldFlip ? 1 : -1), -rcw, Constants.FIELD_RELATIVE);
         drivetrain.drive(driveStates);
     }
