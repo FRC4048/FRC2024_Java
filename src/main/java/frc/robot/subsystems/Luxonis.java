ddbf83551f3280b3b285dfef6d2fa469f1d68fef
bengold22 <98672229+bengold22@users.noreply.github.com>
2024-02-18 16:40:55 -0500

    Adding Luxonis subsystem

---
+++ src/main/java/frc/robot/subsystems/Luxonis.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.smartshuffleboard.SmartShuffleboard;
+
+public class Luxonis extends SubsystemBase {
+
+    NetworkTableEntry xEntry;
+    NetworkTableEntry yEntry;
+    NetworkTableEntry zEntry;
+    NetworkTableEntry fpsEntry;
+    NetworkTableEntry probEntry;
+    
+    public Luxonis() {
+        NetworkTable cameraTable = NetworkTableInstance.getDefault().getTable("Luxonis");
+        xEntry = cameraTable.getEntry("x");
+        yEntry = cameraTable.getEntry("y");
+        zEntry = cameraTable.getEntry("z");
+        fpsEntry = cameraTable.getEntry("fps");
+        probEntry = cameraTable.getEntry("prob");
+    }
+
+    @Override
+    public void periodic() {
+        SmartShuffleboard.put("luxonis", "X", xEntry.getNumber(null));
+        SmartShuffleboard.put("luxonis", "Y", xEntry.getNumber(null));
+        SmartShuffleboard.put("luxonis", "Z", xEntry.getNumber(null));
+        SmartShuffleboard.put("luxonis", "FPS", xEntry.getNumber(null));
+        SmartShuffleboard.put("luxonis", "Prob", xEntry.getNumber(null));
+    }
+}
