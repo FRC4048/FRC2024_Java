0f42382a681720e020d0f128e394c0febbbdd94a
Aditya Hebbar <Adityarhebbar@gmail.com>
2024-02-19 11:49:04 -0500

    Renamed items to reflect that they're in meters

--- src/main/java/frc/robot/commands/drivetrain/MoveDistance.java
+++ src/main/java/frc/robot/commands/drivetrain/MoveDistance.java
@@ -13,20 +13,20 @@ import frc.robot.subsystems.SwerveDrivetrain;
 public class MoveDistance extends Command {
   /** Creates a new MoveDistance. */
   private double startTime;
-  private double changeX;
-  private double changeY;
+  private double changeXMeters;
+  private double changeYMeters;
   private double maxSpeed;
   private double desiredPoseX;
   private double desiredPoseY;
   private SwerveDrivetrain drivetrain;
-  private final double xChangeThreshhold = 0.0762; // TODO: Refine This Number
-  private final double yChangeThreshhold = 0.0762; // TODO: Refine This Number
+  private final double xChangeThreshholdMeters = 0.0762; // TODO: Refine This Number
+  private final double yChangeThreshholdMeters = 0.0762; // TODO: Refine This Number
 
-  public MoveDistance(SwerveDrivetrain drivetrain, double changeX, double changeY, double maxSpeed) {
+  public MoveDistance(SwerveDrivetrain drivetrain, double changeXMeters, double changeYMeters, double maxSpeed) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.drivetrain = drivetrain;
-    this.changeX = changeX;
-    this.changeY = changeY;
+    this.changeXMeters = changeXMeters;
+    this.changeYMeters = changeYMeters;
     this.maxSpeed = Math.abs(maxSpeed);
     addRequirements(drivetrain);
   }
@@ -35,8 +35,8 @@ public class MoveDistance extends Command {
   @Override
   public void initialize() {
     startTime = Timer.getFPGATimestamp();
-    desiredPoseX = drivetrain.getPose().getX() + changeX;
-    desiredPoseY = drivetrain.getPose().getY() + changeY;
+    desiredPoseX = drivetrain.getPose().getX() + changeXMeters;
+    desiredPoseY = drivetrain.getPose().getY() + changeYMeters;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -65,7 +65,7 @@ public class MoveDistance extends Command {
 
     double targetXDistance = Math.abs((drivetrain.getPose().getX() - desiredPoseX));
     double targetYDistance = Math.abs((drivetrain.getPose().getY() - desiredPoseY));
-    if (targetXDistance <= xChangeThreshhold && targetYDistance <= yChangeThreshhold) {
+    if (targetXDistance <= xChangeThreshholdMeters && targetYDistance <= yChangeThreshholdMeters) {
       return true;
     }
     return ((Timer.getFPGATimestamp() - startTime) >= 5);
