1092838afe9ed47ea6223f2a4048b4d6a475d690
Sam Hossler <sam_hossler@outlook.com>
2024-02-21 13:01:12 -0500

    added most of the commands (might be done, not sure)

--- src/main/java/frc/robot/commands/shootcrossline.java
+++ src/main/java/frc/robot/commands/shootcrossline.java
@@ -5,15 +5,30 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.RobotContainer;
+import frc.robot.commands.cannon.Shoot;
+import frc.robot.commands.cannon.StartIntake;
+import frc.robot.commands.drivetrain.MoveDistance;
+import frc.robot.commands.ramp.ResetRamp;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.SwerveDrivetrain;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.Ramp;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class shootcrossline extends SequentialCommandGroup {
   /** Creates a new shootcrossline. */
-  public shootcrossline() {
+  public shootcrossline(SwerveDrivetrain SwerveDrivetrain, Shooter Shooter, Ramp ramp, IntakeSubsystem IntakeSubsystem) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+      new ResetRamp(ramp),
+      new StartIntake(IntakeSubsystem, 6), // adjust later
+      new MoveDistance(SwerveDrivetrain, (RobotContainer.isRedAlliance()?-2.9718:2.9718), 0, 0.3),
+      new Shoot(Shooter),
+      new MoveDistance(SwerveDrivetrain, (RobotContainer.isRedAlliance()?-1.0282:1.0282), 0, 0.3)
+    );
   }
 }
