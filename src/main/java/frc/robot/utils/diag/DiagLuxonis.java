583dc9785fd96f34a369ca47eb65bb29457594a2
Aditya Hebbar <Adityarhebbar@gmail.com>
2024-02-22 18:16:47 -0500

    fixed diags and had them broadcast to shuffleboard

--- src/main/java/frc/robot/utils/diag/DiagLuxonis.java
+++ src/main/java/frc/robot/utils/diag/DiagLuxonis.java
@@ -1,20 +1,38 @@
 package frc.robot.utils.diag;
 
 import edu.wpi.first.networktables.DoubleSubscriber;
-import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import frc.robot.constants.Constants;
 
-public class DiagLuxonis extends DiagBoolean{
-    private final DoubleSubscriber subscriber;
+public class DiagLuxonis implements Diagnosable{
+    private DoubleSubscriber subscriber;
+    private String title;
+    private String name;
+    private GenericEntry networkTableEntry;
+
     public DiagLuxonis(String title, String name){
-        super(title,name);
-        NetworkTableInstance inst = NetworkTableInstance.getDefault();
-        NetworkTable table = inst.getTable("LUXONIS");
-        subscriber = table.getDoubleTopic("Prob").subscribe(0);
+        this.title = title;
+        this.name = name;
+        reset();
+    }
+    protected boolean getDiagResult() {
+        return subscriber.get() > Constants.PIECE_THRESHOLD;
     }
     @Override
-    protected boolean getValue() {
-        return subscriber.get() > Constants.PIECE_THRESHOLD;
+    public void setShuffleBoardTab(ShuffleboardTab shuffleBoardTab, int width, int height) {
+        networkTableEntry = shuffleBoardTab.getLayout(title, BuiltInLayouts.kList).withSize(width, height).add(name, false).getEntry();
+    }
+    @Override
+    public void refresh() {
+        if (networkTableEntry != null) {
+            networkTableEntry.setBoolean(getDiagResult());
+        }
+    }
+    @Override
+    public void reset() {
+        subscriber = NetworkTableInstance.getDefault().getTable("LUXONIS").getDoubleTopic("Prob").subscribe(0);
     }
 }
