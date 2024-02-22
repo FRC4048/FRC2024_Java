ea28ea86ee05180a677078fb3f3fdaa6171efcd7
Sahiltheram <sahil.a.thadani@gmail.com>
2024-02-22 17:13:50 -0500

    Added PID

--- src/main/java/frc/robot/commands/TurnToGamepiece.java
+++ src/main/java/frc/robot/commands/TurnToGamepiece.java
@@ -3,6 +3,7 @@ package frc.robot.commands;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.NetworkTable;
@@ -16,27 +17,19 @@ public class TurnToGamepiece extends Command {
     private SwerveDrivetrain drivetrain;
     // private DoubleSupplier x_position;
     // private DoubleSupplier y_position;
-    private double rcw;
-    private double fwd;
-    private double str;
-    private DoubleSupplier rtSupplier;
     private DoubleSubscriber xSub;
     private DoubleSubscriber ySub;
-    private DoubleSubscriber zSub;
-    private DoubleSubscriber fpsSub;
-    private DoubleSubscriber probSub;
     private double x;
     private double y;
     private double startX;
     private double startY;
-    private double z;
-    private double fps;
-    private double prob;
     ChassisSpeeds driveStates;
     double first = 0;
     double cycle = 0;
     private boolean finished = false;
     double startPose;
+    private final ProfiledPIDController turningPIDController;
+
 
     public TurnToGamepiece(SwerveDrivetrain drivetrain) {
         this.drivetrain = drivetrain;
@@ -45,28 +38,20 @@ public class TurnToGamepiece extends Command {
         NetworkTable table = inst.getTable("Luxonis");
         xSub = table.getDoubleTopic("x").subscribe(-1);
         ySub = table.getDoubleTopic("y").subscribe(-1);
-        zSub = table.getDoubleTopic("z").subscribe(-1);
-        fpsSub = table.getDoubleTopic("fps").subscribe(-1);
-        probSub = table.getDoubleTopic("prob").subscribe(-1);
+        turningPIDController = new ProfiledPIDController(.1, .1, .1);
+
     }
 
     @Override
     public void initialize() {
         super.initialize();
-        double fwd = 0;
-        double str = 0;
-        // double rcw =
-        // MathUtil.applyDeadband(rtSupplier.getAsDouble()*Constants.MAX_VELOCITY, 0.3);
-        double rcw = 0.3;
         first = 0;
         finished = false;
         cycle = 0;
         startPose = drivetrain.getPose().getRotation().getDegrees();
         startX = xSub.get();
         startY = ySub.get();
-        z = zSub.get();
-        fps = fpsSub.get();
-        prob = probSub.get();
+
 
         
 
@@ -90,8 +75,6 @@ public class TurnToGamepiece extends Command {
             }
         }
         else {
-            str = .3;
-            fwd = 0;
             driveStates = drivetrain.createChassisSpeeds(1, 0, 0, false);
             drivetrain.drive(driveStates);
             if (y == 0) {
