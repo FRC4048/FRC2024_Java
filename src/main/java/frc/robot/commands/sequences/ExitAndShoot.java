d36e85ac0a6c70cab4e4c1ab2f300ef257ee1d9b
Soham Patil <sohampatilwest@gmail.com>
2024-02-25 10:13:08 -0500

    errors, copied from main

--- src/main/java/frc/robot/commands/sequences/ExitAndShoot.java
+++ src/main/java/frc/robot/commands/sequences/ExitAndShoot.java
@@ -1,16 +1,23 @@
 package frc.robot.commands.sequences;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.shooter.ShootSpeaker;
 import frc.robot.commands.feeder.FeederGamepieceUntilLeave;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.SwerveDrivetrain;
 
 public class ExitAndShoot extends ParallelCommandGroup{
-    public ExitAndShoot(Shooter shooter, Feeder feeder) {
+    public ExitAndShoot(Shooter shooter, Feeder feeder, SwerveDrivetrain drivetrain) {
         addCommands(
-                new ShootSpeaker(shooter),
+            //Waits for the shooting wheels to go faster, then shoots
+            new ShootSpeaker(shooter, drivetrain),
+            new SequentialCommandGroup(
+                new WaitCommand(.5),
                 new FeederGamepieceUntilLeave(feeder)
+            )
         );
         addRequirements(shooter,feeder);
 
--- src/main/java/frc/robot/commands/sequences/SpoolExitAndShoot.java
+++ src/main/java/frc/robot/commands/sequences/SpoolExitAndShoot.java
@@ -3,16 +3,17 @@ package frc.robot.commands.sequences;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.shooter.ShootSpeaker;
 import frc.robot.commands.feeder.FeederGamepieceUntilLeave;
+import frc.robot.commands.shooter.ShootSpeaker;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.SwerveDrivetrain;
 
 public class SpoolExitAndShoot extends ParallelCommandGroup {
-    public SpoolExitAndShoot(Shooter shooter, Feeder feeder) {
+    public SpoolExitAndShoot(Shooter shooter, Feeder feeder, SwerveDrivetrain drivetrain) {
         addCommands(
-                new ShootSpeaker(shooter),
+                new ShootSpeaker(shooter, drivetrain),
                 new SequentialCommandGroup(
                         new WaitCommand(Constants.SPOOL_TIME),
                         new FeederGamepieceUntilLeave(feeder)
