143b5c0e4aa62ed5bd2fda99125207711045d568
Lucas Vedana <lvdn58@icloud.com>
2024-02-24 17:26:29 -0800

    Added logging for command sequences

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -183,11 +183,11 @@ public class RobotContainer {
         controller.rightBumper().onTrue(CommandUtil.logged(new EngageRatchet(climber)));
 
 //        controller.a().onTrue(new StartFeeder(feeder));
-//        controller.b().onTrue(new ExitAndShoot(shooter,feeder));
+//        controller.b().onTrue(CommandUtil.sequence("Exit And Shoot", new ExitAndShoot(shooter,feeder)));
 //        ramp.setDefaultCommand(new RampMove(ramp, 10));
 //        climber.setDefaultCommand(new ManualClimb(climber, controller::getLeftX));
-        controller.a().onTrue(CommandUtil.logged(new StartIntakeAndFeeder(feeder,intakeSubsystem,deployer,ramp)));
-        controller.b().onTrue(CommandUtil.logged(new ExitAndShoot(shooter,feeder, drivetrain)));
+        controller.a().onTrue(CommandUtil.sequence("Start Intake And Feeder", new StartIntakeAndFeeder(feeder,intakeSubsystem,deployer,ramp)));
+        controller.b().onTrue(CommandUtil.sequence("Exit And Shoot", new ExitAndShoot(shooter,feeder, drivetrain)));;
         controller.x().onTrue(CommandUtil.logged(new LowerDeployer(deployer)));
 //        controller.a().onTrue(new DeployerLower(deployer));
 //        controller.b().onTrue(new DeployerRaise(deployer));
