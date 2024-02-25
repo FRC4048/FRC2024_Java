7f3c7cfa6bd8dc3ac0d40bde1c2188c9da83b050
goatfanboi23 <dev.codetoad@gmail.com>
2024-02-25 12:45:59 -0500

    updated with main and swappedCommand

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,7 +14,6 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.autochooser.chooser.AutoChooser;
--- src/main/java/frc/robot/commands/sequences/StartIntakeAndFeeder.java
+++ src/main/java/frc/robot/commands/sequences/StartIntakeAndFeeder.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.feeder.FeederBackDrive;
 import frc.robot.commands.feeder.StartFeeder;
-import frc.robot.commands.intake.StartIntake;
+import frc.robot.commands.pathplanning.TimedIntake;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -15,7 +15,7 @@ public class StartIntakeAndFeeder extends SequentialCommandGroup{
         addCommands(
             new ParallelDeadlineGroup(
                 new StartFeeder(feeder),
-                new StartIntake(intake, 10)
+                new TimedIntake(intake, 10)
             ),
             new WaitCommand(Constants.FEEDER_BACK_DRIVE_DELAY),
             new FeederBackDrive(feeder)
