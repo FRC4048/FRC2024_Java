538a576645665b408136bf46e20c52b1a32cf178
goatfanboi23 <dev.codetoad@gmail.com>
2024-02-27 20:21:10 -0500

    added example to javadoc

--- src/main/java/frc/robot/utils/command/MotorMoveCommand.java
+++ src/main/java/frc/robot/utils/command/MotorMoveCommand.java
@@ -2,15 +2,24 @@ package frc.robot.utils.command;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import java.util.function.BooleanSupplier;
 import java.util.function.DoubleConsumer;
 
+/**
+ * Fun class for quick testing
+ * example way to do the same functionality as startFeeder <br>
+ * <b>new MotorMoveCommand<>(feeder, 0, feeder::setFeederMotorSpeed, 0.5, feeder::pieceSeen)</b>
+ *
+ */
 public class MotorMoveCommand<T extends SubsystemBase> extends TimedSubsystemCommand<T>{
     private final DoubleConsumer setMotorSpeed;
     private final double speed;
-    public MotorMoveCommand(T subsystem, double timeout, DoubleConsumer setMotorSpeed, double speed) {
+    private final BooleanSupplier finishedCondition;
+    public MotorMoveCommand(T subsystem, double timeout, DoubleConsumer setMotorSpeed, double speed, BooleanSupplier finishedCondition) {
         super(subsystem, timeout);
         this.setMotorSpeed = setMotorSpeed;
         this.speed = speed;
+        this.finishedCondition = finishedCondition;
     }
 
     @Override
@@ -21,5 +30,10 @@ public class MotorMoveCommand<T extends SubsystemBase> extends TimedSubsystemCom
     @Override
     public void end(boolean interrupted) {
         setMotorSpeed.accept(0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return finishedCondition.getAsBoolean() || super.isFinished();
     }
 }
