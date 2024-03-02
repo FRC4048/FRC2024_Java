48a4307cd68403555bf57f536115f1b1ad3e928a
goatfanboi23 <dev.codetoad@gmail.com>
2024-03-01 21:45:13 -0500

    color sensor

--- src/main/java/frc/robot/constants/GameConstants.java
+++ src/main/java/frc/robot/constants/GameConstants.java
@@ -37,7 +37,7 @@ public class GameConstants {
     public static final double SHOOTER_VELOCITY = 6.096;//m/s @ 90%,50%
 
     // FEEDER
-    public static final double COLOR_CONFIDENCE_RATE_INCOMING = .80;
+    public static final double COLOR_CONFIDENCE_RATE_INCOMING = .85;
     public static final double COLOR_CONFIDENCE_RATE_BACKDRIVE = .92;
 
     public static final double MAX_RAMP_ENC = 25.0000;
