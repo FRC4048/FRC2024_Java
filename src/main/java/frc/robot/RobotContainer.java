2c60dfd6a78b4df25763a6bfacdb92e28d664a44
goatfanboi23 <dev.codetoad@gmail.com>
2024-03-05 20:05:59 -0500

    added limeLight path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -127,6 +127,7 @@ public class RobotContainer {
         NamedCommands.registerCommand("RampShootComboCenter", CommandUtil.logged(new RampShootCombo(ramp,shooter, Constants.RAMP_CENTER_AUTO_SHOOT)));// second piece
         NamedCommands.registerCommand("RampShootComboSide", CommandUtil.logged(new RampShootCombo(ramp,shooter, Constants.RAMP_SIDE_AUTO_SHOOT))); // first and third
         NamedCommands.registerCommand("RampShootComboSide2", CommandUtil.logged(new RampShootCombo(ramp,shooter,Constants.RAMP_DIP_AUTO_SHOOT))); // first and third
+        NamedCommands.registerCommand("MoveToGamePiece", CommandUtil.logged(new TurnToGampieceGroup(vision,drivetrain,feeder,intake,deployer,ramp)));
     }
 
     private void setupPathPlanning() {
