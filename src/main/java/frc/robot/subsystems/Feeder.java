b3ea172238f2cd1c77856b6d2d03f8e7817e5262
goatfanboi23 <dev.codetoad@gmail.com>
2024-03-10 23:12:54 -0400

    multithreading color sensor

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.ColorMatchResult;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -13,18 +14,31 @@ import frc.robot.utils.ColorValue;
 import frc.robot.utils.diag.DiagColorSensor;
 import frc.robot.utils.smartshuffleboard.SmartShuffleboard;
 
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Feeder extends SubsystemBase {
 
     private final WPI_TalonSRX feederMotor;
     private final I2C.Port i2cPort = I2C.Port.kMXP;
     private final ColorSensor colorSensor;
+    private final BlockingQueue<ColorMatchResult> colorReadingBuffer = new LinkedBlockingQueue<>();
+    private final ArrayList<ColorMatchResult> resultsSinceLastTic = new ArrayList<>();
+
 
     public Feeder() {
         this.feederMotor = new WPI_TalonSRX(Constants.FEEDER_MOTOR_ID);
         this.feederMotor.setNeutralMode(NeutralMode.Brake);
-
         colorSensor = new ColorSensor(i2cPort);
         Robot.getDiagnostics().addDiagnosable(new DiagColorSensor("Feeder", "Color Sensor", colorSensor));
+        new Thread(() -> {
+            try {
+                colorReadingBuffer.put(colorSensor.getMatchedColor());
+            } catch (InterruptedException e) {
+                DriverStation.reportError("Color Sensor put call interrupted", e.getStackTrace());
+            }
+        }).start();
     }
 
     public void setFeederMotorSpeed(double speed) {
@@ -47,13 +61,19 @@ public class Feeder extends SubsystemBase {
     }
 
     public boolean pieceSeen(boolean incoming) {
-        ColorMatchResult latestResult = colorSensor.getMatchedColor();
-        double confidence = incoming ? Constants.COLOR_CONFIDENCE_RATE_INCOMING : Constants.COLOR_CONFIDENCE_RATE_BACKDRIVE;
-        return latestResult.confidence > confidence;
+        for (ColorMatchResult match: resultsSinceLastTic){
+            double confidence = incoming ? Constants.COLOR_CONFIDENCE_RATE_INCOMING : Constants.COLOR_CONFIDENCE_RATE_BACKDRIVE;
+            if (match.confidence > confidence){
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void periodic() {
+        resultsSinceLastTic.clear();
+        colorReadingBuffer.drainTo(resultsSinceLastTic);
         if (Constants.FEEDER_DEBUG) {
             ColorValue detectedColor = colorSensor.getColor();
             Color rawColor = colorSensor.getRawColor();
