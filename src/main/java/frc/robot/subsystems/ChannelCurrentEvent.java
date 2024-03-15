0ef193f59fab70af581c643c95aae6d4f017f747
goatfanboi23 <dev.codetoad@gmail.com>
2024-03-15 00:23:25 -0400

    update with comments. (not done)

--- src/main/java/frc/robot/subsystems/ChannelCurrentEvent.java
+++ src/main/java/frc/robot/subsystems/ChannelCurrentEvent.java
@@ -1,28 +1,34 @@
 package frc.robot.subsystems;
 
 import java.util.Objects;
+import java.util.function.DoublePredicate;
 
 public class ChannelCurrentEvent {
     private final int channel;
-    private final double currentThreshold;
+    private final DoublePredicate currentThreshold;
     private final Runnable action;
+    private final boolean persistent;
 
-    public ChannelCurrentEvent(int channel, double currentThreshold, Runnable action) {
+    public ChannelCurrentEvent(int channel, DoublePredicate currentThreshold, Runnable action, boolean persistent) {
         this.channel = channel;
         this.currentThreshold = currentThreshold;
         this.action = action;
+        this.persistent = persistent;
     }
 
     public int getChannel() {
         return channel;
     }
 
-    public double getCurrentThreshold() {
+    public DoublePredicate getCurrentThreshold() {
         return currentThreshold;
     }
 
     public Runnable getAction() {
         return action;
+    }
+    public boolean shouldPersist(){
+        return persistent;
     }
 
     @Override
@@ -30,11 +36,11 @@ public class ChannelCurrentEvent {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ChannelCurrentEvent that = (ChannelCurrentEvent) o;
-        return channel == that.channel && currentThreshold == that.currentThreshold && Objects.equals(action, that.action);
+        return channel == that.channel && persistent == that.persistent && Objects.equals(currentThreshold, that.currentThreshold) && Objects.equals(action, that.action);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(channel, currentThreshold, action);
+        return Objects.hash(channel, currentThreshold, action, persistent);
     }
 }
--- src/main/java/frc/robot/subsystems/PowerMonitor.java
+++ src/main/java/frc/robot/subsystems/PowerMonitor.java
@@ -7,16 +7,15 @@ import frc.robot.constants.Constants;
 import frc.robot.utils.logging.Logger;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.BitSet;
 import java.util.List;
-import java.util.Set;
+import java.util.function.DoublePredicate;
 
 public class PowerMonitor extends SubsystemBase {
     private final String loggingBase = "/robot/pdh/";
     private final PowerDistribution pdh;
-    private final Set<Integer> monitoringChannels = new HashSet<>();
-    private final List<ChannelCurrentEvent> maxCurrentEvents = new ArrayList<>();
-    private final List<ChannelCurrentEvent> minCurrentEvents = new ArrayList<>();
+    private final BitSet monitoringChannels = new BitSet();
+    private final List<ChannelCurrentEvent> currentEvents = new ArrayList<>();
 
     public PowerMonitor() {
         this.pdh = new PowerDistribution(1, PowerDistribution.ModuleType.kRev);
@@ -28,56 +27,74 @@ public class PowerMonitor extends SubsystemBase {
      */
     public boolean logChannel(int channel) {
         if (channel < pdh.getNumChannels()) {
-            return monitoringChannels.add(channel);
+            monitoringChannels.set(channel, true);
+            return true;
         }
         return false;
     }
 
     /**
      * @param channel to unmonitor
-     * @return boolean representing if removal from monitoring set was successful
      */
-    public boolean unlogChannel(int channel) {
-        return monitoringChannels.remove(channel);
+    public void unlogChannel(int channel) {
+        monitoringChannels.set(channel, false);
     }
 
     /**
-     * Runs action when channel current draw reaches a threshold. <br>
-     * Note, when channel reaches current draw you will have to re-register the event
-     *
-     * @param channel          to monitor
-     * @param currentThreshold current threshold before runnable is called
-     * @param runnable         action to preform when current is reached
+     * Runs action when channel current draw goes below or above a threshold.
+     * @param channel       to monitor
+     * @param shouldTrigger predicate that takes a current and returns true if you want event to be fired or false if you want the event to not be fired
+     * @param runnable      action to preform when event triggers
+     * @param persistent    should the event persist or be removed after called once
      * @return if addition to currentEvent list was successful
      */
-    public boolean onCurrentAboveThreshold(int channel, double currentThreshold, Runnable runnable) {
+    public boolean onCurrentChange(int channel, DoublePredicate shouldTrigger, Runnable runnable, boolean persistent) {
         if (channel < pdh.getNumChannels()) {
-            return maxCurrentEvents.add(new ChannelCurrentEvent(channel, currentThreshold, runnable));
+            return currentEvents.add(new ChannelCurrentEvent(channel, shouldTrigger, runnable, persistent));
         }
         return false;
     }
-
     /**
-     * Runs action when channel current draw goes below a threshold. <br>
-     * Note, when channel goes under min current draw you will have to re-register the event
-     *
-     * @param channel          to monitor
-     * @param currentThreshold current threshold before runnable is called
-     * @param runnable         action to preform when current is reached
+     * Runs action when channel current draw goes below or above a threshold.
+     * @param event to add
      * @return if addition to currentEvent list was successful
      */
-    public boolean onCurrentBelowThreshold(int channel, double currentThreshold, Runnable runnable) {
+    public boolean onCurrentChange(ChannelCurrentEvent event) {
+        if (event.getChannel() < pdh.getNumChannels()) {
+            return currentEvents.add(event);
+        }
+        return false;
+    }
+    /**
+     * Removes {@link ChannelCurrentEvent} from monitoring list
+     * @param channel       to monitor
+     * @param shouldTrigger predicate that takes a current and returns true if you want event to be fired or false if you want the event to not be fired
+     * @param runnable      action to preform when event triggers
+     * @param persistent    should the event persist or be removed after called once
+     * @return if removal to currentEvent list was successful
+     */
+    public boolean ignoreCurrentChange(int channel, DoublePredicate shouldTrigger, Runnable runnable, boolean persistent) {
         if (channel < pdh.getNumChannels()) {
-            return minCurrentEvents.add(new ChannelCurrentEvent(channel, currentThreshold, runnable));
+            return currentEvents.remove(new ChannelCurrentEvent(channel, shouldTrigger, runnable, persistent));
+        }
+        return false;
+    }
+    /**
+     * Removes {@link ChannelCurrentEvent} from monitoring list
+     * @param event to remove
+     * @return if removal to currentEvent list was successful
+     */
+    public boolean ignoreCurrentChange(ChannelCurrentEvent event) {
+        if (event.getChannel() < pdh.getNumChannels()) {
+            return currentEvents.remove(event);
         }
         return false;
     }
 
     @Override
     public void periodic() {
-        callCurrentMaxEvents();
-        callCurrentMinEvents();
-        monitoringChannels.forEach(c -> {
+        callCurrentEvents();
+        monitoringChannels.stream().forEach(c -> {
             Logger.logDouble(loggingBase + "channel" + c + "Current", getChannelCurrent(c), Constants.ENABLE_LOGGING);
         });
         Logger.logDouble(loggingBase + "totalCurrent", getTotalCurrent(), Constants.ENABLE_LOGGING);
@@ -91,29 +108,20 @@ public class PowerMonitor extends SubsystemBase {
         return pdh.getVoltage();
     }
 
-    private void callCurrentMaxEvents() {
-        List<ChannelCurrentEvent> events = maxCurrentEvents.stream().filter(event -> {
+    private void callCurrentEvents() {
+        List<ChannelCurrentEvent> events = currentEvents.stream().filter(event -> {
             int channel = event.getChannel();
-            return getChannelCurrent(channel) > event.getCurrentThreshold();
+            return event.getCurrentThreshold().test(getChannelCurrent(channel));
         }).toList();
         events.forEach(e -> e.getAction().run());
-        maxCurrentEvents.removeAll(events);
-    }
-
-    private void callCurrentMinEvents() {
-        List<ChannelCurrentEvent> events = minCurrentEvents.stream().filter(event -> {
-            int channel = event.getChannel();
-            return getChannelCurrent(channel) < event.getCurrentThreshold();
-        }).toList();
-        events.forEach(e -> e.getAction().run());
-        minCurrentEvents.removeAll(events);
+        currentEvents.removeAll(events.stream().filter(e -> !e.shouldPersist()).toList());
     }
 
     /**
      * @param channel to check
      * @return current value from channel or {@link Double#NaN} if channel is invalid
      */
-    public double getChannelCurrent(int channel){
+    public double getChannelCurrent(int channel) {
         if (channel < pdh.getNumChannels()) {
             return pdh.getCurrent(channel);
         }
@@ -123,33 +131,36 @@ public class PowerMonitor extends SubsystemBase {
     /**
      * @return total Current draw from pdh
      */
-    public double getTotalCurrent(){
+    public double getTotalCurrent() {
         return pdh.getTotalCurrent();
     }
-    public PowerDistributionFaults getFaults(){
+
+    public PowerDistributionFaults getFaults() {
         return pdh.getFaults();
     }
 
     /**
      * Power is the bus voltage multiplied by the current with the units Watts.
+     *
      * @return Total power drawn from the pdh.
      */
-    public double getTotalPower(){
+    public double getTotalPower() {
         return pdh.getTotalPower();
     }
 
     /**
      * Energy is the power summed over time with units Joules.
+     *
      * @return Total Energy drawn from the pdh
      */
-    public double getTotalEnergy(){
+    public double getTotalEnergy() {
         return pdh.getTotalEnergy();
     }
 
     /**
      * @return Temperature in degrees celsius
      */
-    public double getTemp(){
+    public double getTemp() {
         return pdh.getTemperature();
     }
 }
