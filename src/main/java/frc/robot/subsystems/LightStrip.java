d1453f7e299f00921a4250150783bbf8fcf9977e
goatfanboi23 <dev.codetoad@gmail.com>
2024-03-21 11:09:04 -0400

    multi-thread led strip (needs to be tested. Might improve command scheduler loop overrun)

--- src/main/java/frc/robot/subsystems/LightStrip.java
+++ src/main/java/frc/robot/subsystems/LightStrip.java
@@ -1,49 +1,39 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.Constants;
 import frc.robot.utils.BlinkinPattern;
-import frc.robot.utils.logging.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BooleanSupplier;
 
 public class LightStrip extends SubsystemBase {
     private static final String baseLogName = "/robot/lightstrip/";
     private final Spark colorSensorPort;
-    private BlinkinPattern pattern = BlinkinPattern.WHITE;
+    private BlinkinPattern pattern = BlinkinPattern.BLACK;
     private final Map<BooleanSupplier, BlinkinPattern> predicateLightEvents = new HashMap<>();
-
     public LightStrip(int port) {
         this.colorSensorPort = new Spark(port);
-    }
-
-    @Override
-    public void periodic() {
-        predicateLightEvents.keySet()
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> predicateLightEvents.keySet()
                 .stream()
                 .filter(BooleanSupplier::getAsBoolean)
                 .findFirst()
                 .ifPresent(c -> {
-                    setPattern(predicateLightEvents.get(c));
-                    predicateLightEvents.remove(c);
-                    Logger.logInteger(baseLogName + "predicateSize", predicateLightEvents.size(), Constants.ENABLE_LOGGING);
-                }
-        );
+                            setPattern(predicateLightEvents.get(c));
+                            predicateLightEvents.remove(c);
+                        }
+                ),0,40, TimeUnit.MILLISECONDS);
     }
 
-    public void setPattern(BlinkinPattern pattern) {
+    public synchronized void setPattern(BlinkinPattern pattern) {
         this.pattern = pattern;
         colorSensorPort.set(pattern.getPwm());
-        SmartDashboard.putString("PATTERN",pattern.toString());
-        Logger.logDouble(baseLogName + "pwmSignal", pattern.getPwm(), Constants.ENABLE_LOGGING);
-        Logger.logString(baseLogName + "pwmName", pattern.toString(), Constants.ENABLE_LOGGING);
     }
 
-    public BlinkinPattern getPattern() {
+    public synchronized BlinkinPattern getPattern() {
         return pattern;
     }
     public void scheduleOnTrue(BooleanSupplier callable, BlinkinPattern pattern) {
