b6104d6d6631f37188342f1a8247073ec61391f3
goatfanboi23 <dev.codetoad@gmail.com>
2024-03-22 10:09:10 -0400

    removed amp

--- src/main/java/frc/robot/commands/sequences/CancelAllSequence.java
+++ src/main/java/frc/robot/commands/sequences/CancelAllSequence.java
@@ -2,16 +2,13 @@ package frc.robot.commands.sequences;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.CancelAll;
-import frc.robot.commands.amp.RetractAmp;
-import frc.robot.subsystems.Amp;
 import frc.robot.subsystems.LightStrip;
 import frc.robot.subsystems.Ramp;
 import frc.robot.subsystems.Shooter;
 
 public class CancelAllSequence extends SequentialCommandGroup {
-    public CancelAllSequence(Ramp ramp, Shooter shooter, Amp amp, LightStrip lightStrip) {
+    public CancelAllSequence(Ramp ramp, Shooter shooter, LightStrip lightStrip) {
         addCommands(
-            new RetractAmp(amp, lightStrip),
             new CancelAll(ramp, shooter, lightStrip)
         );
     }
