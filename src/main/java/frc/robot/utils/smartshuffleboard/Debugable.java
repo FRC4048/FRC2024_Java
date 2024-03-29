c715678c1f64710d50a7cf612271a11d890be970
goatfanboi23 <dev.codetoad@gmail.com>
2024-03-29 11:23:26 -0400

    log cache flush time

--- src/main/java/frc/robot/utils/smartshuffleboard/Debugable.java
+++ src/main/java/frc/robot/utils/smartshuffleboard/Debugable.java
@@ -3,12 +3,15 @@ package frc.robot.utils.smartshuffleboard;
 import edu.wpi.first.networktables.*;
 import frc.robot.constants.Constants;
 import frc.robot.utils.CachedCallback;
+import frc.robot.utils.logging.Logger;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public abstract class Debugable<T> {
@@ -29,44 +32,53 @@ public abstract class Debugable<T> {
             instance.addListener(entry, EnumSet.of(NetworkTableEvent.Kind.kPublish), (event -> addToCache(getUpdate(event))));
         }
     }
+
+    public static void flushCache() {
+        Instant start = Instant.now();
+        CachedCallback<?> poll = callbackCache.poll();
+        while (poll != null) {
+            poll.call();
+            poll = callbackCache.poll();
+        }
+        int diff = (int) TimeUnit.MICROSECONDS.convert(Instant.now().getNano() - start.getNano(), TimeUnit.NANOSECONDS);
+        Logger.logInteger("robot/debugableCacheTime", diff, Constants.ENABLE_LOGGING);
+    }
+
     public void addListener(Consumer<T> consumer) {
         List<Consumer<?>> list = callbacks.getOrDefault(entry.getTopic(), new ArrayList<>());
         list.add(consumer);
         callbacks.put(entry.getTopic(), list);
     }
 
-    protected List<Consumer<?>> getCallback(Topic topic){
+    protected List<Consumer<?>> getCallback(Topic topic) {
         return callbacks.get(topic);
     }
-    public void addCallback(Topic topic, Consumer<T> consumer){
+
+    public void addCallback(Topic topic, Consumer<T> consumer) {
         List<Consumer<?>> list = callbacks.getOrDefault(topic, new ArrayList<>());
         list.add(consumer);
         callbacks.put(topic, list);
-    }
-    public static void flushCache(){
-        CachedCallback<?> poll = callbackCache.poll();
-        while (poll != null){
-            poll.call();
-            poll = callbackCache.poll();
-        }
     }
 
     public T getDefaultValue() {
         return defaultValue;
     }
-    public void resetValue(){
+
+    public void resetValue() {
         entry.setValue(defaultValue);
     }
 
-    protected void addToCache(CachedCallback<T> callback){
-        if (callback != null){
+    protected void addToCache(CachedCallback<T> callback) {
+        if (callback != null) {
             callbackCache.add(callback);
         }
     }
+
     protected abstract CachedCallback<T> getUpdate(NetworkTableEvent event);
 
     protected abstract void setLastValue(T defaultValue);
 
-    protected abstract void setValue(T defaultValue);
     public abstract T getValue();
+
+    protected abstract void setValue(T defaultValue);
 }
