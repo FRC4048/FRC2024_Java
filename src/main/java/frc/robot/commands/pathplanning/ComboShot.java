cc76af423489ca8ac4f6bca57b2db866a53ec06f
bengold22 <98672229+bengold22@users.noreply.github.com>
2024-03-30 09:07:20 -0400

    Adding timeout to shoot commands

--- src/main/java/frc/robot/commands/pathplanning/ComboShot.java
+++ src/main/java/frc/robot/commands/pathplanning/ComboShot.java
@@ -1,8 +1,9 @@
 package frc.robot.commands.pathplanning;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.feeder.TimedFeeder;
-import frc.robot.commands.shooter.StopShooter;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.LightStrip;
@@ -11,9 +12,11 @@ import frc.robot.subsystems.Shooter;
 public class ComboShot extends ParallelCommandGroup {
     public ComboShot(Shooter shooter, Feeder feeder, LightStrip lightStrip) {
         addCommands(
-                new BasicShoot(shooter, lightStrip,1),
-                new TimedFeeder(feeder, lightStrip, Constants.TIMED_FEEDER_EXIT),
-                new StopShooter(shooter)
+                new BasicShoot(shooter, lightStrip, 1.7),
+                new SequentialCommandGroup(
+                    new WaitCommand(.5),
+                    new TimedFeeder(feeder, lightStrip, Constants.TIMED_FEEDER_EXIT)
+                )
         );
     }
 
--- src/main/java/frc/robot/commands/pathplanning/ShootAndDrop.java
+++ src/main/java/frc/robot/commands/pathplanning/ShootAndDrop.java
@@ -5,7 +5,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.deployer.LowerDeployer;
 import frc.robot.commands.feeder.TimedFeeder;
-import frc.robot.commands.shooter.StopShooter;
 import frc.robot.constants.Constants;
 import frc.robot.subsystems.Deployer;
 import frc.robot.subsystems.Feeder;
@@ -14,11 +13,13 @@ import frc.robot.subsystems.Shooter;
 
 public class ShootAndDrop extends ParallelDeadlineGroup {
     public ShootAndDrop(Shooter shooter, Feeder feeder, Deployer deployer, LightStrip lightStrip) {
-        super(new BasicShoot(shooter, lightStrip,1),
+        super(new BasicShoot(shooter, lightStrip, 1.7),
                 new SequentialCommandGroup(
                         new WaitCommand(0.5),
-                        new TimedFeeder(feeder, lightStrip, Constants.TIMED_FEEDER_EXIT),
-                        new StopShooter(shooter)
+                        new SequentialCommandGroup(
+                            new WaitCommand(.5),
+                            new TimedFeeder(feeder, lightStrip, Constants.TIMED_FEEDER_EXIT)
+                        )
                 ),
                 new LowerDeployer(deployer, lightStrip));
     }
