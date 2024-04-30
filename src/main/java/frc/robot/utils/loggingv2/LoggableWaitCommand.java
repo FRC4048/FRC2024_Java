7b02d63e182b9800b23a2545713cb0a3ec2ce4f6
goatfanboi23 <dev.codetoad@gmail.com>
2024-04-29 21:48:08 -0400

    manually init wait command

--- src/main/java/frc/robot/utils/loggingv2/LoggableWaitCommand.java
+++ src/main/java/frc/robot/utils/loggingv2/LoggableWaitCommand.java
@@ -1,14 +1,20 @@
 package frc.robot.utils.loggingv2;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 
-public class LoggableWaitCommand extends WaitCommand implements Loggable {
+
+//cant use extends WaitCommand because if nameChange SendableRegistry
+public class LoggableWaitCommand extends Command implements Loggable {
+    private final Timer timer;
+    private final double seconds;
     private String basicName = getClass().getSimpleName();
     private Command parent = new BlankCommand();
 
+
     public LoggableWaitCommand(double seconds) {
-        super(seconds);
+        this.seconds = seconds;
+        timer = new Timer();
     }
 
     @Override
@@ -34,4 +40,24 @@ public class LoggableWaitCommand extends WaitCommand implements Loggable {
         this.basicName = name;
         return this;
     }
+    @Override
+    public void initialize() {
+        timer.restart();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        timer.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return timer.hasElapsed(seconds);
+    }
+
+    @Override
+    public boolean runsWhenDisabled() {
+        return true;
+    }
+
 }
