2ff57f494bb75794060663bec6aa13187db6f4e7
codetoad <softwareenginer@pm.me>
2024-12-05 19:40:34 -0500

    deleted stuff

--- src/main/java/frc/robot/subsystems/swervev3/bags/ModuleInputsStamped.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.swervev3.bags;
-
-public record ModuleInputsStamped(double steerEncoderPosition, double driveEncoderPosition,
-                                  double driveEncoderVelocity, double steerEncoderVelocity,
-                                  double measurementTimestamp) {
-}
